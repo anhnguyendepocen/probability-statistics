5f89bee4024672c8dad6a026d055cebb0a4fe1b6
chico <fxchiang@gmail.com>
2018-04-23 14:48:09 -0300

    implemented python program to calculate binomial distribution probabilities

---
+++ utils/binomial_distribution.py
@@ -0,0 +1,42 @@
+from math import factorial as fac
+from math import pow
+
+
+'''
+Calculate probabilities of a random variable with binomial distribution
+'''
+
+def binomial(k, n, p):
+    '''
+    input
+        k - the number of successes in trials
+        n - the number of trials
+        p - the probability of a sucess
+    returns
+        P(X = k)
+    '''
+    fail_count = n - k
+    success_count = fac(n)/(fac(k)*fac(fail_count))
+    success_prob = pow(p, k)
+    fail_prob = pow(1 - p, fail_count)
+    return success_count * success_prob * fail_prob
+
+
+def binlt(k, n, p):
+    # P(X < k)
+    return sum(binomial(x, n, p) for x in range(0, k))
+
+
+def binle(k, n, p):
+    # P(X <= k)
+    return sum(binomial(x, n, p) for x in range(0, k+1))
+
+
+def bingt(k, n, p):
+    # P(X > k)
+    return 1 - binle(k, n, p)
+
+
+def binge(k, n, p):
+    # P(X >= k)
+    return 1 - binlt(k, n, p)
